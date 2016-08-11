8563bda202173fecb48eb567a275c9de1f989c80
Mike Reed <mike.reed.vallejo@gmail.com>
2016-08-11 14:12:48 -0700

    add foundation UI format to all pages

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome!!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations</p>
+      <ol>
+        <li>
+          <Link to='/?city=Vallejo'>Vallejo, CA</Link>
+        </li>
+        <li>
+          <Link to='/?city=Paris'>Paris, France</Link>
+        </li>
+      </ol>
     </div>
   )
 };
